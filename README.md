f2fd874a9dab4b2991392342bbb45e89443a2179
gepanago <geo.png1997@gmail.com>
2025-06-24 14:41:36 +0300

    update readme.md

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # StackExchange-Statistics-Service
-a Python-based REST API service that retrieves data from the StackExchange API, calculates specific statistics, and reports the results
+a Python-based REST API service that retrieves data from the StackExchange API, calculates specific statistics, and reports the results.
+
+### Project Goals
+ - Provide a REST API endpoint to retrieve StackOverflow answer data within a specified date/time range.
+ - Calculate and return specific statistics based on the retrieved data.
+
+ #### Functional Requirements 
+ 1. Expose a REST API endpoint at:
+GET http://localhost:5000/api/v1/stackstats?since=<datetime>&until=<datetime>
+ 2. Accept two datetime parameters (since and until) in the format:
+YYYY-MM-DD HH:MM:SS
+ 3. Retrieve StackOverflow answers within the specified date/time range using the StackExchange API (1st call).
+ 4. Retrieve comments associated with these answers (2nd call).
+ 5. Calculate the following:
+    * Total number of **accepted** answers.
+    * Average score of the **accepted** answers.
+    * Average answer count per question.
+    * Comment count for each of the top 10 answers with the highest scores.
+ 6. Cache the results for faster reapeated retrievals.
+ 
+  #### Non-Functional Requirements 
+  1. Python version: >=3.6
+
+
+### Key Components - Stackexchange API Client
+- **`src/app/components/stackexchange.py`**: Contains the `StackExchangeClient` class for interacting with the StackExchange API. It supports fetching answers and comments with pagination and batching.
+- **`notebooks/stackexchange_notebook.ipynb`**: A Jupyter Notebook demonstrating the usage of the `StackExchangeClient` and testing its functionality.
+- **`.gitignore`**: Specifies files and directories to be ignored by Git.
+- **`requirements.txt`**: Lists Python dependencies required for the project.
+- **`README.md`**: Documentation for the project.
---
+++ src/app/components/stackexchange.py
@@ -0,0 +1,84 @@
+import requests
+
+def _make_paginated_request(url: str, params: dict):
+    """
+    Handle paginated API requests.
+    """
+    results = []
+    has_more = True
+    page = 1
+
+    while has_more:
+        params["page"] = page
+        try:
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            print(f"Error fetching data from StackExchange API, url:{url}, page: {page} : {e}")
+            return []
+        data = response.json()
+        results.extend(data.get("items", []))
+        has_more = data.get("has_more", False)
+        page += 1
+
+    return results
+
+class StackExchangeClient:
+    """
+    Handles integration with the StackExchange API for retrieving answers and comments.
+    """
+    BASE_URL = "https://api.stackexchange.com/2.3/"
+    ANSWERS_URL = f"{BASE_URL}answers"
+    SITE = "stackoverflow"
+    PAGESIZE = 100
+
+    def get_answers(self, since, until):
+        """
+        Fetches answers from StackExchange within the specified date range.
+        Params:
+            - since (int): Start date as a Unix timestamp.
+            - until (int): End date as a Unix timestamp.
+        Returns:
+            - List of answers with relevant details.
+        """
+        url = self.ANSWERS_URL
+        params = {
+            "fromdate": since,
+            "todate": until,
+            "site": self.SITE,
+            "pagesize": self.PAGESIZE,
+            "order": "asc",
+            "sort": "creation"
+        }
+        return _make_paginated_request(url, params)
+
+    def get_comments(self, answer_ids: list, batch_size: int = 80):
+        """
+        Fetches comments for a specific answer.
+        Params:
+            - answer_ids (list): The IDs of the answer to fetch comments for.
+        Returns:
+            - List of comments associated with the answer(s).
+        """
+        def _get_comments_for_batch(batch):
+            concatenated_ids = ";".join(map(str, batch))
+
+            url = f"{self.ANSWERS_URL}/{concatenated_ids}/comments"
+            params = {
+                "order": "asc",
+                "sort": "creation",
+                "site": self.SITE
+            }
+            return _make_paginated_request(url, params)
+
+        if not answer_ids:
+            return []
+        if len(answer_ids) <= batch_size:
+            return _get_comments_for_batch(answer_ids)
+        else:
+            print(f"Warning: Too many answer IDs provided ({len(answer_ids)}), switch to batch mode with batch size {batch_size}.")
+        comments = []
+        for i in range(0, len(answer_ids), batch_size):
+            batch = answer_ids[i:i + batch_size]
+            comments.extend(_get_comments_for_batch(batch))
+        return comments
