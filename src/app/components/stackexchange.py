834d05d0f609ebdd3ab612c7801fddef2500a89b
George Panagopoulos <94235344+PanagopoulosGeorge@users.noreply.github.com>
2025-06-26 17:58:53 +0300

    Update stackexchange.py

--- src/app/components/stackexchange.py
+++ src/app/components/stackexchange.py
@@ -1,84 +1,110 @@
+from typing import List, Dict, Optional
 import requests
+from decouple import config
+from .rate_limits import rate_limited, throttle_backoff_limited
+from cachetools import TTLCache, cached
+from cachetools.keys import hashkey
+"""
+Rate Limits:
+ - 30 requests/second per IP
+ - 10,000 requests/day per API key --> (not covered, can acquire more keys and play with random.choice)
+ - Dynamic backoff throttling per method: If an application receives a response with the backoff field set, it must wait that many seconds before hitting the same method again.
+ - Heavy caching (don't repeat identical requests within 1 minute)
+"""
 
-def _make_paginated_request(url: str, params: dict):
-    """
-    Handle paginated API requests.
-    """
-    results = []
-    has_more = True
-    page = 1
+MAX_PAGES = 24
+DEFAULT_BATCH = 60 * 60 * 24 # 1 day
+ANSWERS_SHORT_CACHING = TTLCache(maxsize=256, ttl=60)
+COMMENTS_SHORT_CACHING = TTLCache(maxsize=256, ttl=60)
 
-    while has_more:
-        params["page"] = page
-        try:
-            response = requests.get(url, params=params)
-            response.raise_for_status()
-        except requests.RequestException as e:
-            print(f"Error fetching data from StackExchange API, url:{url}, page: {page} : {e}")
-            return []
-        data = response.json()
-        results.extend(data.get("items", []))
-        has_more = data.get("has_more", False)
-        page += 1
-
-    return results
+class StackExchangeError(Exception): ...
 
 class StackExchangeClient:
-    """
-    Handles integration with the StackExchange API for retrieving answers and comments.
-    """
-    BASE_URL = "https://api.stackexchange.com/2.3/"
-    ANSWERS_URL = f"{BASE_URL}answers"
-    SITE = "stackoverflow"
-    PAGESIZE = 100
+    def __init__(self, api_key: Optional[str] = None, session: Optional[requests.Session] = None):
+        self.__api_key = api_key or config("STACK_EXCHANGE_API_KEY", default=None)
+        self.session = session or requests.Session(
+        ## establish possibly a proxy or other configs
+        )
+        self.BASE_URL = "https://api.stackexchange.com/2.3"
+        self.SITE = "stackoverflow"
 
-    def get_answers(self, since, until):
-        """
-        Fetches answers from StackExchange within the specified date range.
-        Params:
-            - since (int): Start date as a Unix timestamp.
-            - until (int): End date as a Unix timestamp.
-        Returns:
-            - List of answers with relevant details.
-        """
-        url = self.ANSWERS_URL
-        params = {
-            "fromdate": since,
-            "todate": until,
-            "site": self.SITE,
-            "pagesize": self.PAGESIZE,
-            "order": "asc",
-            "sort": "creation"
-        }
-        return _make_paginated_request(url, params)
+    def get_answers(self, start_date_unix: int, end_date_unix: int,
+                          batch: int = DEFAULT_BATCH, mock_api: bool = False) -> List[Dict]:
+        answers: List[Dict] = []
+        if mock_api:
+            url = "https://gist.githubusercontent.com/PanagopoulosGeorge/4a5b2c1304971e502d64a5c1b13248bb/raw/6b748538ebeb137597655514a7dd47547d387f35/gistfile1.txt"
+            response = self.session.get(url)
+            results = response.json()['items']
+            return results
+        for batch_start in self._iterate_batches(start_date_unix, end_date_unix, batch):
+            batch_end = min(batch_start + batch - 1, end_date_unix)
+            answers.extend(self._fetch_paginated(
+                f"{self.BASE_URL}/answers",
+                {
+                    "site": self.SITE,
+                    "fromdate": batch_start,
+                    "todate":   batch_end,
+                    "order": "asc",
+                    "sort":  "creation",
+                    **({"key": self.__api_key} if self.__api_key else {}),
+                },
+                object_to_fetch = 'answer'
+            ))
+        return answers
 
-    def get_comments(self, answer_ids: list, batch_size: int = 80):
-        """
-        Fetches comments for a specific answer.
-        Params:
-            - answer_ids (list): The IDs of the answer to fetch comments for.
-        Returns:
-            - List of comments associated with the answer(s).
-        """
-        def _get_comments_for_batch(batch):
-            concatenated_ids = ";".join(map(str, batch))
-
-            url = f"{self.ANSWERS_URL}/{concatenated_ids}/comments"
-            params = {
-                "order": "asc",
-                "sort": "creation",
-                "site": self.SITE
-            }
-            return _make_paginated_request(url, params)
-
-        if not answer_ids:
-            return []
-        if len(answer_ids) <= batch_size:
-            return _get_comments_for_batch(answer_ids)
-        else:
-            print(f"Warning: Too many answer IDs provided ({len(answer_ids)}), switch to batch mode with batch size {batch_size}.")
-        comments = []
-        for i in range(0, len(answer_ids), batch_size):
-            batch = answer_ids[i:i + batch_size]
-            comments.extend(_get_comments_for_batch(batch))
+    def get_comments(self, answer_ids: List[int], batch_size: int = 90) -> List[Dict]:
+        comments: List[Dict] = []
+        for i in self._iterate_batches(0, len(answer_ids), batch_size):
+            ids = ";".join(map(str, answer_ids[i: i + batch_size]))
+            url = f"{self.BASE_URL}/answers/{ids}/comments"
+            comments.extend(self._fetch_paginated(url, {"site": self.SITE}, object_to_fetch = 'comment'))
         return comments
+
+    @staticmethod
+    def _iterate_batches(start: int, end: int, delta: int):
+        cur = start
+        while cur < end:
+            yield cur
+            cur += delta
+
+    def _fetch_paginated(self, url: str, base_params: dict, object_to_fetch = 'answer') -> List[Dict]:
+        items, page = [], 1
+        while True:
+            params = {**base_params, "page": page}
+            data = self.__fetch(url, params=params, timeout=10, object_to_fetch = object_to_fetch)
+            items.extend(data.get("items", []))
+            if not data.get("has_more", False):
+                break
+            if page >= MAX_PAGES and not self.__api_key:
+                raise StackExchangeError("Exceeded max page limit")
+            page += 1
+        return items
+
+    @rate_limited(max_per_second=30)
+    def __fetch(self, url, params=None, timeout=10, object_to_fetch='answer'):
+        data = self.__fetch_answer(url, params=params, timeout=timeout) if object_to_fetch == 'answer' else self.__fetch_comment(url, params=params, timeout=timeout)
+        return data
+
+    @throttle_backoff_limited
+    @cached(cache=ANSWERS_SHORT_CACHING,
+            key=lambda *args, **kwargs: hashkey(args[0], tuple(sorted(kwargs.get('params', {}).items()))))
+    def __fetch_answer(self, url, params=None, timeout=10):
+        response = self.session.get(url, params=params, timeout=timeout)
+        try:
+            response.raise_for_status()
+        except requests.HTTPError as exc:
+            raise StackExchangeError(str(exc)) from exc
+        return response.json()
+
+
+    @throttle_backoff_limited
+    @cached(cache=COMMENTS_SHORT_CACHING,
+            key=lambda *args, **kwargs: hashkey(args[0], tuple(sorted(kwargs.get('params', {}).items()))))
+
+    def __fetch_comment(self, url, params=None, timeout=10):
+        response = self.session.get(url, params=params, timeout=timeout)
+        try:
+            response.raise_for_status()
+        except requests.HTTPError as exc:
+            raise StackExchangeError(str(exc)) from exc
+        return response.json()
